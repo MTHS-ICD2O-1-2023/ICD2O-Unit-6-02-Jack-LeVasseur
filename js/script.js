62538a284d3760084e1e9e765a7faaebe8c8c0b7
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-24 18:07:57 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+let clicks = parseInt(localStorage.getItem("click-counter")) || 0
+
+function updateCookieCount() {
+
+  document.getElementById("click-count").innerHTML = clicks
+}
+
+function myButtonClicked() {
+  clicks += 1
+
+  // Update the HTML element with the updated clicks value
+  document.getElementById("click-count").innerHTML = clicks
+
+  // Update the clicks value in local storage
+  localStorage.setItem("click-counter", clicks)
+}
